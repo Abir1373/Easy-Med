ed738ae2862d6135fe108f29820bf8f3076622fe
Abir1373 <abirm3188@gmail.com>
2024-02-25 22:42:25 +0600

    new correction

--- src/Components/Appointment/SeleceServices.jsx
+++ src/Components/Appointment/SeleceServices.jsx
@@ -3,6 +3,7 @@ import Servicecard from "./Servicecard";
 
 export default function SeleceServices() {
     const [doctors, refetch] = getDoctors()
+    let specialities = []
     let mp = {}
     if (doctors && doctors.length) {
         for (let i = 0; i < doctors.length; i++) {
@@ -11,16 +12,21 @@ export default function SeleceServices() {
             }
         }
     }
+    specialities = Object.keys(mp);
+    console.log(specialities)
+    if(!specialities || specialities.length===0)
+    {
+        <span className="loading loading-ball loading-lg"></span>
+    }
+
     return (
         <div className="my-8">
             <p className="text-center text-[#F7A582]">Available Services on April 30, 2022  </p>
             <h2 className="font-bold text-center text-4xl">Please select a service.</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center mt-5 gap-4">
-                {doctors ? doctors.map(item => <Servicecard key={item._id} doctors={item} />
-
-                )
-                    :
-                    <progress className="progress progress-success w-56" value="100" max="100"></progress>}
+                {
+                    specialities.map((item,index)=><Servicecard key={index} item={item}></Servicecard>)
+                }
             </div>
         </div>
     )
--- src/Components/Appointment/Servicecard.jsx
+++ src/Components/Appointment/Servicecard.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 
-export default function Servicecard({ value}) {
+export default function Servicecard({item}) {
+    console.log(item)
     return (
         <div className='border-2 border-green-600 rounded-md text-center px-3 py-4'>
-            <Link to={`/services/${value}`} className="text-green-900 font-bold text-xl cursor-pointer hover:underline">{value}</Link>
+            <Link to={`/services/${item}`} className="text-green-900 font-bold text-xl cursor-pointer hover:underline">{item}</Link>
         </div >
     )
 }
